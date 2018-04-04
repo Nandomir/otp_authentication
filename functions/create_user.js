adb2c7b42e7230028ab979cacc60aafecee890dd
Nandomir <f.sendyka@gmail.com>
2018-04-04 23:51:17 +0100

    Udpated createUser function. Function now verifies the number, formats it and creates a new user with that number in Firebase.

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,22 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
-  res.send(req.body);   // body is a JS object containing all the data passed to this function when user called it
+  // Verify the user provided a phone number
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });   
+    // 422 gives a message that something wrong happened
+  }
+
+  // Format the phone number to remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g);
+
+  // Create a new user account using that number
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
+    // uid = property used to identify unique user, this statement returns a promise, therefore the use of .then statement
+
+  // Respond to the user account, saying the account was made
+
+
 }
--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,12 @@
 const admin = require('firebase-admin'); //allows accessing the data in Firebase
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
+const serviceAccount = require('./service_account.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount), 
   databaseURL: "https://one-time-password-f40d4.firebaseio.com"
 });
+// service account is the private key generated from Firebase as a .json file
 
 exports.createUser = functions.https.onRequest(createUser);
