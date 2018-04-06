eb5abc9ade0d6d82c1d6141deb024ee67f27aa44
Nandomir <f.sendyka@gmail.com>
2018-04-06 20:00:23 +0100

    Updated twilio.messages.create to handle cases when code is not generated correctly. Updated twilio.messages.create method to actually generate the code sent to the user and flag the generated code as used.

--- functions/request_one_time_password.js
+++ functions/request_one_time_password.js
@@ -12,11 +12,23 @@ module.exports = function(req, res) {
     .then(userRecord => {
       const code = Math.floor((Math.random() * 8999 + 1000)); // floor guarantees we will get a rounded number
 
+      // twilio api
       twilio.messages.create({
         from: '+441315103691',
         to: phone,
         body: 'You recieved an SMS from Node.js using Twilio. Your code is ' + code
-      }) // twilio api
+      }, (err) => {
+        //handling the case when the code is not sent correctly
+        if (err) { return res.status(422).send(err); } 
+        // in production better use this 
+        // { error: 'An error occured while sending a code. }
+        
+        admin.database().ref('users/' + phone) // generating the code here
+          .update({ code: code, codeValid: true }, () => { // saving the code
+            res.send({ success: true }); // successfuly generated a code and send it
+          });
+
+      })
     })
     .catch((err) => {
       res.status(422).send({ error: err}); // res.status(422).send({ error: 'User not found'}); ideal in a production world
