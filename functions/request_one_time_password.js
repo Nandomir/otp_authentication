e2e79f9fc151b17b1f402aa26f6c6143eff704c2
Nandomir <f.sendyka@gmail.com>
2018-04-07 23:35:37 +0100

    Added return null; to admin.auth().getUser(phone) method, as Eslint enforces the use of catch() on un-returned promises.

--- functions/request_one_time_password.js
+++ functions/request_one_time_password.js
@@ -9,14 +9,14 @@ module.exports = function(req, res) {
   const phone = String(req.body.phone).replce(/[^\d.]/g, '');
 
   admin.auth().getUser(phone)  // asychronus request
-    .then(userRecord => {
+    .then(userRecord =>  {
       const code = Math.floor((Math.random() * 8999 + 1000)); // floor guarantees we will get a rounded number
 
       // twilio api
       twilio.messages.create({
+        body: 'You recieved an SMS from Node.js using Twilio. Your code is ' + code,
         from: '+441315103691',
-        to: phone,
-        body: 'You recieved an SMS from Node.js using Twilio. Your code is ' + code
+        to: phone
       }, (err) => {
         //handling the case when the code is not sent correctly
         if (err) { return res.status(422).send(err); } 
@@ -27,8 +27,9 @@ module.exports = function(req, res) {
           .update({ code: code, codeValid: true }, () => { // saving the code
             res.send({ success: true }); // successfuly generated a code and send it
           });
-
+      
       })
+      return null;
     })
     .catch((err) => {
       res.status(422).send({ error: err}); // res.status(422).send({ error: 'User not found'}); ideal in a production world
